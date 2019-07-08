778fbaf2b8e93f4b502f211d9f8c3ea6a88f049c
Alex Lopez <alejandro.lopez@amarello.com.mx>
2019-07-08 16:55:53 -0500

    Se agrego la base para el nuevo sitio

---
+++ js/scroll.js
@@ -0,0 +1,66 @@
+
+$(window).scroll(function() {
+    if($(window).scrollTop() < 240) {
+        $("header").addClass("active");
+    } else {
+       $("header").removeClass("active");
+    }
+});
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").classList.add('minum');
+    document.getElementById("header").classList.add('water');
+    document.getElementById("header").classList.add('basic');
+    console.log('scroll')
+  } else {
+    document.getElementById("header").classList.remove('minum');
+    document.getElementById("header").classList.remove('water');
+    document.getElementById("header").classList.remove('basic');
+  }
+}
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+
+}
+

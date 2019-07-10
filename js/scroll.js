7b4cb9d4d0e4bb272ebd54dafdeaa61163d530a8
Alex Lopez <alejandro.lopez@amarello.com.mx>
2019-07-10 18:45:04 -0500

    Desarrollando el menu principal con estilos

--- js/scroll.js
+++ js/scroll.js
@@ -52,11 +52,13 @@ function hasScrolled() {
     // This is necessary so you never see what is "behind" the navbar.
     if (st > lastScrollTop && st > navbarHeight){
         // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
+        // $('header').removeClass('nav-down').addClass('nav-up');
+        $('header').addClass('nav-up');
     } else {
         // Scroll Up
         if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
+            // $('header').removeClass('nav-up').addClass('nav-down');
+            $('header').removeClass('nav-up');
         }
     }
     

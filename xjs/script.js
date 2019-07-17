d5d8f4f5df82f254dc708038bac2adcdf095b75c
Alex Lopez <alejandro.lopez@amarello.com.mx>
2019-07-17 18:46:37 -0500

    Cambios recientes segunda semana

---
+++ xjs/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+
+
+	$(".menu-btn").on( "click", function() {
+	  if($(this).attr("trigger")==="0"){                
+		  	 $(this).animate({"right":"200px"},200);
+	         $(this).attr("trigger","1");
+	 }
+	  else{
+	    $(this).animate({"right":"0px"},200);
+        $(this).attr("trigger","0");
+	  }
+	});
+
+});

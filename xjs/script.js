f0ce83b3c9a089f420e25d3ef64b3f9889292747
Alex Lopez <alejandro.lopez@amarello.com.mx>
2019-07-18 18:33:27 -0500

    Se hicieron detalles al menu responsive

--- xjs/script.js
+++ xjs/script.js
@@ -4,15 +4,24 @@ $(document).ready(function(){
 	});
 
 
-	$(".menu-btn").on( "click", function() {
-	  if($(this).attr("trigger")==="0"){                
-		  	 $(this).animate({"right":"200px"},200);
-	         $(this).attr("trigger","1");
-	 }
-	  else{
-	    $(this).animate({"right":"0px"},200);
-        $(this).attr("trigger","0");
-	  }
-	});
+	// $(".menu-btn").on( "click", function() {
+	//   if($(this).attr("trigger")==="0"){                
+	// 	  	 $(this).animate({"right":"200px"},200);
+	//          $(this).attr("trigger","1");
+	//  }
+	//   else{
+	//     $(this).animate({"right":"0px"},200);
+ //        $(this).attr("trigger","0");
+	//   }
+	// });
+
+
+		$(".site-overlay").on( "click", function() {
+           $("#nav-icon1").removeClass('open');
+		});
+
+		$(".pushy-content").on( "click", function() {
+           $("#nav-icon1").removeClass('open');
+		});
 
 });

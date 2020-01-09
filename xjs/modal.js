88146ef88ac3d5151aca6737c9744635cd834b4f
Ricardo Taboada <rtaboada@amarello.com.mx>
2020-01-09 17:00:11 -0600

    intergraion del correo

--- xjs/modal.js
+++ xjs/modal.js
@@ -1,21 +1,54 @@
 /*     Modal       */
+$(function () {
+  $("form[name='formulario-contacto']").validate({
+    // Specify validation rules
+    rules: {
+      choose: "required",
+      name: "required",
+      company: "required",
+      message: "required",
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 5
+      }
+    },
+    messages: {
+      name: "Please enter your name",
+      email: "Please enter a valid email address",
+      company: "Please enter your company",
+      message: "Please enter your message",
+    },
 
+    submitHandler: function (form, event) {
+      event.preventDefault();
+      $.post('https://backend-usuarios-sqdcgxmbqq-uc.a.run.app/landing/sendMessage',$(form).serialize())
+        .done(function() {
+          $(".mask").addClass("active");
+        })
+        .fail(function(data) {
+          alert(data.responseJSON.message)
+        })
+    }
+  });
 
-$(".showy").on("click", function(){
-  $(".mask").addClass("active");
-});
-
-
-function closeModal(){
-  $(".mask").removeClass("active");
-}
-
-$(".close, .mask").on("click", function(){
-  closeModal();
-});
-
-$(document).keyup(function(e) {
-  if (e.keyCode == 27) {
-    closeModal();
+  function closeModal() {
+    $(".mask").removeClass("active");
   }
-});
+
+  $(".close, .mask").on("click", function () {
+    closeModal();
+  });
+
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) {
+      closeModal();
+    }
+  });
+
+})
+
+

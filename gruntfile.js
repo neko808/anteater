e6983eee81909aa77cfb2da5085f99900eab2925
Alex Lopez <alejandro.lopez@amarello.com.mx>
2020-01-09 12:31:00 -0600

    Se arreglo el Formulario con Jquery

--- gruntfile.js
+++ gruntfile.js
@@ -1,38 +1,40 @@
 
 module.exports = function(grunt){
 
-	//Configuration
-	grunt.initConfig({
-		//pass in options to plugins, references to files, etc.
-        sass: { 
-           dev: {
-	          files:{
-	            'styles/main.css': 'styles/main.scss'
-	          }
-           }
-        }
+//Configuration
+grunt.initConfig({
+  sass: {                              // Task
+    dist: {                            // Target
+      files: {                         // Dictionary of files
+        'styles/main.css': 'styles/main.scss'       // 'destination': 'source'  
+      }
+    }
+  }
+});
+ 
 
-	});
 
-
-	// Load up tasks
-    grunt.loadNpmTasks('grunt-contrib-sass');
-
+//Load up tasks
+grunt.loadNpmTasks('grunt-contrib-sass');
 
 
 
 
-	//Register Tasks
-	grunt.registerTask('run', function(){
-      console.log('I am running');
-	});
+//Register Tasks
 
-	grunt.registerTask('sleep', function(){
-      console.log('I am sleeping');
-	});
+grunt.registerTask('sass', ['sass']);
+	
+
+	// grunt.registerTask('run', function(){
+ //      console.log('I am running');
+	// });
+
+	// grunt.registerTask('sleep', function(){
+ //      console.log('I am sleeping');
+	// });
 
 
-	grunt.registerTask('all', ['sleep','run']);
+	// grunt.registerTask('all', ['sleep','run']);
 
 
 

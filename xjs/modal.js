edfdd0a7bba1e4a6680e103c63e754409b055021
Alex Lopez <alejandro.lopez@amarello.com.mx>
2020-01-10 12:10:20 -0600

    Guardando cambios para hacer merge

--- xjs/modal.js
+++ xjs/modal.js
@@ -1,3 +1,44 @@
+//Vars
+const elegir = document.getElementById('elegir');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const empresa = document.getElementById('empresa');
+const message = document.getElementById('message');
+const btnEnviar = document.getElementById('enviar');
+
+
+//event listeners
+
+eventListeners();
+
+function eventListeners(){
+
+    document.addEventListener('DomContentLoaded', inicioApp);
+
+
+}
+
+
+
+
+
+
+//functions
+
+function inicioApp(){
+
+	btnEnviar.disabled = true;
+
+
+}
+
+
+
+
+
+
+
+
 /*     Modal       */
 
 
